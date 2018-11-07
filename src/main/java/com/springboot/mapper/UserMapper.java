fa9c1b76806f5dda9c88d18aae41b1510566e24e
li <854378082@qq.com>
2018-11-07 18:24:52 +0800

    mybatis

---
+++ src/main/java/com/springboot/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package com.springboot.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.springboot.entity.User;
+
+@Mapper
+public interface UserMapper {
+	/**
+	 * 根据用户名查询用户结果集
+     *
+     * @param username 用户名
+     * @return 查询结果
+     */
+    @Select("SELECT * FROM t_user WHERE username = #{username}")
+    List<User> findByUsername(@Param("username") String username);
+
+
+    /**
+ 	* 保存用户信息
+     *
+     * @param user 用户信息
+     * @return 成功 1 失败 0
+     */
+    int insert(User user);
+}
