f9649492545c0f73735226f8f484d43c7c0115d1
li <854378082@qq.com>
2018-11-18 21:19:47 +0800

    session start

---
+++ src/main/java/com/coins/configure/RedisSessionConfig.java
@@ -0,0 +1,10 @@
+package com.coins.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisSessionConfig {
+
+}
