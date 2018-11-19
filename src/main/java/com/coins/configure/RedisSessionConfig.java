373ac968cde6b47d6461fb9a22a0a880a5f9d37a
li <854378082@qq.com>
2018-11-19 17:18:49 +0800

    session config

--- src/main/java/com/coins/configure/RedisSessionConfig.java
+++ src/main/java/com/coins/configure/RedisSessionConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableRedisHttpSession
+//在这里设置Session过期时间，单位：秒
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 7200)
 public class RedisSessionConfig {
 
 }
