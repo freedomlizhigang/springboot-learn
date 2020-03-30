8147544c9b5c7474f81aa2089f9f60a24ec6abbf
smz_li <lzhigang@shanmuzhi.com>
2020-03-30 16:49:12 +0800

    security 关闭session

--- src/main/java/com/coins/configure/RedisSessionConfig.java
+++ src/main/java/com/coins/configure/RedisSessionConfig.java
@@ -1,11 +1,11 @@
 package com.coins.configure;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 //在这里设置Session过期时间，单位：秒
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 7200)
+// @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 7200)
 public class RedisSessionConfig {
-
+	
 }
--- src/main/java/com/coins/controller/home/HomeController.java
+++ src/main/java/com/coins/controller/home/HomeController.java
@@ -1,7 +1,5 @@
 package com.coins.controller.home;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,25 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping
 public class HomeController {
 	@GetMapping("/")
-    public ModelAndView index(@RequestParam("browser") String browser,HttpSession session) {
+    public ModelAndView index(@RequestParam("browser") String browser) {
 		ModelAndView view = new ModelAndView();
 		// 设置跳转的视图 默认映射到 src/main/resources/templates/{viewName}.html
         view.setViewName("index");
         // 设置属性
         view.addObject("title", "我的第一个WEB页面");
         view.addObject("desc", "欢迎进入battcn-web 系统");
-        session.setAttribute("fds", "这个是试试Session可不可以用的");
-        
-      //取出session中的browser
-        Object sessionBrowser = session.getAttribute("browser");
-        if (sessionBrowser == null) {
-            System.out.println("不存在session，设置browser=" + browser);
-            session.setAttribute("browser", browser);
-        } else {
-            System.out.println("存在session，browser=" + sessionBrowser.toString());
-        }
-        
-        view.addObject("aaa",session.getAttribute("browser").toString());
         return view;
     }
 	
