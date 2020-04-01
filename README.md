775a58a0b20074129e71b67f9f5ae34b856947be
smz_li <lzhigang@shanmuzhi.com>
2020-04-01 19:51:51 +0800

    mybatis plus end

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # springboot-learn
 学习springboot
+1. springboot(web/test/configuration-processor/jdbc/mysql-connector/security)
+2. mybatis,mybatis plus,数据库操作，代码生成
+3. lombok:数据注解等功能，简化get/set
--- src/main/java/com/coins/controller/rbac/SectionsController.java
+++ src/main/java/com/coins/controller/rbac/SectionsController.java
@@ -1,9 +1,17 @@
 package com.coins.controller.rbac;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.coins.entity.rbac.Sections;
+import com.coins.mapper.rbac.SectionsMapper;
 
 /**
  * <p>
@@ -16,6 +24,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/sections")
 public class SectionsController {
-
+	@Autowired
+	private SectionsMapper sectionMapper;
+	
+	@GetMapping("/list")
+	public Sections listSection()
+	{
+		Sections list = sectionMapper.selectById(1);
+		return list;
+	}
 }
 
