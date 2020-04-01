b9f313dfc2d4069625de8874421bb2562dad2be7
smz_li <lzhigang@shanmuzhi.com>
2020-04-01 15:45:57 +0800

    mybatis plus generator

--- src/main/java/com/coins/CoinsApplication.java
+++ src/main/java/com/coins/CoinsApplication.java
@@ -1,12 +1,12 @@
 package com.coins;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.RestController;
 
-import tk.mybatis.spring.annotation.MapperScan;
 
 @RestController
 @SpringBootApplication(exclude={HibernateJpaAutoConfiguration.class})
