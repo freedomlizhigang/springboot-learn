d6a8b145b4d5f58bd5807b617c8a13a20fe3486e
smz_li <lzhigang@shanmuzhi.com>
2020-04-04 09:18:57 +0800

    验证器

--- src/main/java/com/coins/entity/rbac/Sections.java
+++ src/main/java/com/coins/entity/rbac/Sections.java
@@ -4,6 +4,12 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 
 import java.time.LocalDateTime;
+
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import java.io.Serializable;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -26,13 +32,20 @@ public class Sections implements Serializable {
 
 	@TableId(value = "id", type = IdType.AUTO)
     private Integer id;
-
+    
+    @NotNull(message = "名称不能为空")
     private String name;
 
+    @NotNull(groups = updateStatus.class,message = "状态必填")
+    @Min(value = 1,groups = updateStatus.class)
+    @Max(3)
     private Integer status;
 
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
 
+    // 分组校验
+    public interface updateStatus {
+    }
 }
