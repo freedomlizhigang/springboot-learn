c17bb15faf9ac46be600af9b259c2a56706033e9
smz_li <lzhigang@shanmuzhi.com>
2020-04-21 09:45:16 +0800

    validated reqeust

--- src/main/java/com/coins/controller/rbac/SectionsController.java
+++ src/main/java/com/coins/controller/rbac/SectionsController.java
@@ -10,11 +10,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coins.entity.rbac.Sections;
 import com.coins.mapper.rbac.SectionsMapper;
+import com.coins.request.rbac.SectionListRequest;
 import com.coins.utils.CommonResult;
 import com.coins.utils.ResultUtil;
 
@@ -33,11 +34,7 @@ public class SectionsController {
 	private SectionsMapper sectionMapper;
 	
 	@GetMapping("/all")
-	public CommonResult getAll(@Validated Sections section,BindingResult result) {
-		if(result.hasErrors())
-		{
-			return ResultUtil.error(400, result.getAllErrors().get(0).getDefaultMessage());
-		}
+	public CommonResult getAll(SectionListRequest section) {
 		List<Sections> all = sectionMapper.selectList(null);
 		return ResultUtil.success(200, "success", all);
 	}
@@ -46,7 +43,7 @@ public class SectionsController {
 	 * 分组验证 Validated(Sections.updateStatus.class)，对于没有标记分组的不起作用
 	 */
 	@GetMapping("/list")
-	public CommonResult getList(@Validated(Sections.updateStatus.class) Sections section, BindingResult result) {
+	public CommonResult getList(@Validated(Sections.updateStatus.class) Sections section, BindingResult result,SectionListRequest sectionreq) {
 		if (result.hasErrors()) {
 			return ResultUtil.error(400, result.getAllErrors().get(0).getDefaultMessage());
 		}
