75804108551bd32b705f1f44a6c323dfb6d0e8b4
smz_li <lzhigang@shanmuzhi.com>
2020-05-04 22:16:21 +0800

    管理员基础

--- src/main/java/com/coins/rbac/entity/Admins.java
+++ src/main/java/com/coins/rbac/entity/Admins.java
@@ -1,9 +1,22 @@
 package com.coins.rbac.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import java.time.LocalDateTime;
 import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -24,31 +37,55 @@ public class Admins implements Serializable {
     private static final long serialVersionUID=1L;
 
     @TableId(value = "id", type = IdType.AUTO)
+    @NotNull(groups = {updateAdmin.class,updatePwd.class},message = "ID必填")
     private Integer id;
-
+    
+    @NotNull(groups = {updateAdmin.class,createAdmin.class},message = "部门不能为空")
     private Integer sectionId;
-
+    
+    @NotEmpty(groups = {createAdmin.class},message = "用户名不能为空")
+    @Null(groups = {updateAdmin.class,updatePwd.class},message = "用户名不传")
     private String name;
-
+    
+    @NotNull(groups = {updateAdmin.class,createAdmin.class},message = "真实姓名不能为空")
     private String realname;
-
+    
+    @NotNull(groups = {updateAdmin.class,createAdmin.class},message = "邮箱不能为空")
+    @Pattern(regexp = "^([a-zA-Z0-9]*[-_]?[a-zA-Z0-9]+)*@([a-zA-Z0-9]*[-_]?[a-zA-Z0-9]+)+[\\.][A-Za-z]{2,3}([\\.][A-Za-z]{2})?$",groups = {updateAdmin.class,createAdmin.class},message = "邮箱格式")
     private String email;
-
+    
+    @NotNull(groups = {updatePwd.class,createAdmin.class},message = "密码不能为空")
+    @Length(groups = {updatePwd.class,createAdmin.class},min=6,max=15,message = "密码长度6-15位")
+    @Null(groups = {updateAdmin.class},message = "密码不传")
     private String password;
 
     private String crypt;
-
+    
+    @NotNull(groups = {updateAdmin.class,createAdmin.class},message = "手机号不能为空")
+    @Pattern(regexp = "^1[3456789]\\d{9}$",groups = {updateAdmin.class,createAdmin.class},message = "手机号格式不正确")
     private String phone;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime lasttime;
 
     private String lastip;
-
+    
+    @Range(min=0,max=1,groups= {updateAdmin.class,createAdmin.class})
     private Integer status;
-
+    
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createdAt;
-
+    
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updatedAt;
 
-
+    // 分组校验
+    public interface updateAdmin {
+    }
+    public interface updatePwd {
+    }
+    public interface createAdmin {
+    }
 }
